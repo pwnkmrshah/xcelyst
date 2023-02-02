7ac42ae606e0ca13aeaffcf2ac8ddebd86297cac
Nishu Jain <nishu.j@jointworksolutions.com>
2023-02-02 12:56:03 +0530

    Update assets config

--- hiringplatform-ruby/config/environments/development.rb
+++ hiringplatform-ruby/config/environments/development.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
   config.active_storage.service = :amazon
+  config.assets.compress = true
+  config.assets.debug = true
   config.assets.compile = true
 
   # Don't care if the mailer can't send.
